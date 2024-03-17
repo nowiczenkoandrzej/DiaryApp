778c3b94242cc4acf0622246273ce28c88b95fbf
Andrzej <t4r4boom@gmail.com>
2024-03-17 23:20:29 +0100

    poprawienie bottom sheet

--- app/src/main/java/com/an/diaryapp/feature_note_list/presentation/NotesListScreen.kt
+++ app/src/main/java/com/an/diaryapp/feature_note_list/presentation/NotesListScreen.kt
@@ -3,6 +3,7 @@ package com.an.diaryapp.feature_note_list.presentation
 import android.util.Log
 import android.util.Range
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,14 +14,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -34,6 +36,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -42,12 +45,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -64,11 +66,13 @@ import com.an.diaryapp.feature_note_list.presentation.components.ListCategory
 import com.an.diaryapp.feature_note_list.presentation.components.DeleteNoteDialog
 import com.an.diaryapp.feature_note_list.presentation.components.NoteListItem
 import com.maxkeppeker.sheets.core.models.base.rememberSheetState
-import com.maxkeppeler.sheets.calendar.CalendarDialog
 import com.maxkeppeler.sheets.calendar.CalendarView
 import com.maxkeppeler.sheets.calendar.models.CalendarConfig
 import com.maxkeppeler.sheets.calendar.models.CalendarSelection
 import com.maxkeppeler.sheets.calendar.models.CalendarStyle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
@@ -98,12 +102,11 @@ fun NotesListScreen(
         mutableStateOf(false)
     }
 
+
     val focusManager = LocalFocusManager.current
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    val calendarStateFromDate = rememberSheetState()
-    val calendarStateToDate = rememberSheetState()
     val mainCalendarState = rememberSheetState()
 
     val selectedDateRange = remember {
@@ -125,23 +128,35 @@ fun NotesListScreen(
         titleContentColor = MaterialTheme.colorScheme.onPrimary
     )
 
+
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    val scope = rememberCoroutineScope()
+
+
+
+
     LaunchedEffect(lifecycleState) {
-        when(lifecycleState) {
+        when (lifecycleState) {
             Lifecycle.State.DESTROYED -> {}
             Lifecycle.State.INITIALIZED -> {}
-            Lifecycle.State.CREATED -> {viewModel.onEvent(NoteListEvent.GetNotes)}
+            Lifecycle.State.CREATED -> {
+                viewModel.onEvent(NoteListEvent.GetNotes)
+            }
+
             Lifecycle.State.STARTED -> {}
-            Lifecycle.State.RESUMED -> {viewModel.onEvent(NoteListEvent.GetNotes)}
+            Lifecycle.State.RESUMED -> {
+                viewModel.onEvent(NoteListEvent.GetNotes)
+            }
         }
     }
     LaunchedEffect(listState.isScrollInProgress) {
-        if(listState.isScrollInProgress) {
+        if (listState.isScrollInProgress) {
 
             focusManager.clearFocus(true)
         }
     }
 
-    if(showDeleteNoteDialog) {
+    if (showDeleteNoteDialog) {
         DeleteNoteDialog(
             onDismissRequest = {
                 showDeleteNoteDialog = false
@@ -159,28 +174,78 @@ fun NotesListScreen(
     }
 
 
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
 
-    CalendarDialog(
-        state = calendarStateFromDate,
-        config = CalendarConfig(
-            monthSelection = true,
-            yearSelection = true
-        ),
-        selection = CalendarSelection.Date { date ->
-           viewModel.onEvent(NoteListEvent.SelectFilterFromDate(date))
-        }
-    )
+                Spacer(modifier = Modifier.height(16.dp))
 
-    CalendarDialog(
-        state = calendarStateToDate,
-        config = CalendarConfig(
-            monthSelection = true,
-            yearSelection = true
-        ),
-        selection = CalendarSelection.Date { date ->
-            viewModel.onEvent(NoteListEvent.SelectFilterToDate(date))
-        }
-    )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    HorizontalDivider(
+                        color = MaterialTheme.colorScheme.primary,
+                        thickness = 1.dp,
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Text(
+                        text = "Date range",
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 8.dp)
+                    )
+
+                    HorizontalDivider(
+                        color = MaterialTheme.colorScheme.primary,
+                        thickness = 1.dp,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+
+                CalendarView(
+                    sheetState = mainCalendarState,
+                    config = CalendarConfig(
+                        style = CalendarStyle.MONTH
+                    ),
+                    selection = CalendarSelection.Period(
+                        selectedRange = selectedDateRange.value
+                    ) { startDate, endDate ->
+                        selectedDateRange.value = Range(startDate, endDate)
+                    }
+                )
+
+                CalendarView(
+                    sheetState = mainCalendarState,
+                    config = CalendarConfig(
+                        style = CalendarStyle.MONTH
+                    ),
+                    selection = CalendarSelection.Period(
+                        selectedRange = selectedDateRange.value
+                    ) { startDate, endDate ->
+                        selectedDateRange.value = Range(startDate, endDate)
+                    }
+                )
+
+
+                Spacer(modifier = Modifier.height(40.dp))
+
+            }
+        },
+        sheetPeekHeight = 0.dp
+
+    ) {
 
     Scaffold(
         modifier = Modifier
@@ -206,7 +271,10 @@ fun NotesListScreen(
                 },
                 actions = {
                     IconButton(onClick = {
-                        isSheetOpen = true
+                        //isSheetOpen = true
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
                     }) {
                         Icon(
                             painter = painterResource(id = R.drawable.baseline_filter_alt_24),
@@ -252,7 +320,7 @@ fun NotesListScreen(
                         items(notes) { note ->
                             NoteListItem(
                                 noteItem = note,
-                                onClick =  {
+                                onClick = {
                                     navController.navigate(
                                         Screen.NoteDetails.passId(note.id!!)
                                     )
@@ -267,88 +335,94 @@ fun NotesListScreen(
                     }
                 }
             }
+
+
         }
-    }
-
-    if(isSheetOpen) {
-        ModalBottomSheet(
-            sheetState = sheetState,
-            onDismissRequest = { isSheetOpen = false }
-        ) {
-
-            
-            Column(
-                modifier = Modifier.fillMaxWidth()
+        /*if(isSheetOpen) {
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = { isSheetOpen = false },
+                dragHandle = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Filters",
+                            fontSize = 24.sp
+                        )
+                    }
+                }
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "Filters",
-                        fontSize = 24.sp
-                    )
-                }
 
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Row(
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-
+                        .fillMaxSize()
                 ) {
-                    HorizontalDivider(
-                        color = MaterialTheme.colorScheme.primary,
-                        thickness = 1.dp,
-                        modifier = Modifier.weight(1f)
-                    )
 
-                    Text(
-                        text = "Date range",
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Row(
                         modifier = Modifier
-                            .padding(start = 8.dp, end = 8.dp)
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        HorizontalDivider(
+                            color = MaterialTheme.colorScheme.primary,
+                            thickness = 1.dp,
+                            modifier = Modifier.weight(1f)
+                        )
+
+                        Text(
+                            text = "Date range",
+                            modifier = Modifier
+                                .padding(start = 8.dp, end = 8.dp)
+                        )
+
+                        HorizontalDivider(
+                            color = MaterialTheme.colorScheme.primary,
+                            thickness = 1.dp,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+
+                    CalendarView(
+                        sheetState = mainCalendarState,
+                        config = CalendarConfig(
+                            style = CalendarStyle.MONTH
+                        ),
+                        selection = CalendarSelection.Period(
+                            selectedRange = selectedDateRange.value
+                        ) { startDate, endDate ->
+                            selectedDateRange.value = Range(startDate, endDate)
+                        }
                     )
 
-                    HorizontalDivider(
-                        color = MaterialTheme.colorScheme.primary,
-                        thickness = 1.dp,
-                        modifier = Modifier.weight(1f)
+                    CalendarView(
+                        sheetState = mainCalendarState,
+                        config = CalendarConfig(
+                            style = CalendarStyle.MONTH
+                        ),
+                        selection = CalendarSelection.Period(
+                            selectedRange = selectedDateRange.value
+                        ) { startDate, endDate ->
+                            selectedDateRange.value = Range(startDate, endDate)
+                        }
                     )
+
+
+                    Spacer(modifier = Modifier.height(40.dp))
+
                 }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                CalendarView(
-                    sheetState = mainCalendarState,
-                    config = CalendarConfig(
-                        style = CalendarStyle.MONTH
-                    ),
-                    selection = CalendarSelection.Period(
-                        selectedRange = selectedDateRange.value
-                    ) { startDate, endDate ->
-                        selectedDateRange.value = Range(startDate, endDate)
-                    }
-                )
-
-                CalendarView(
-                    sheetState = mainCalendarState,
-                    config = CalendarConfig(
-                        style = CalendarStyle.MONTH
-                    ),
-                    selection = CalendarSelection.Period(
-                        selectedRange = selectedDateRange.value
-                    ) { startDate, endDate ->
-                        selectedDateRange.value = Range(startDate, endDate)
-                    }
-                )
-
-
-                Spacer(modifier = Modifier.height(40.dp))
-
             }
-        }
+        }*/
     }
+}
+
 }
