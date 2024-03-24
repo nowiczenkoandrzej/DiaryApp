573b15f46c1c7ea32ab3b3914b32c628d737fbd4
Andrzej <t4r4boom@gmail.com>
2024-03-25 00:02:41 +0100

    dodanie animacji do ekranu slonecznej pogody

--- app/src/main/java/com/an/diaryapp/feature_note_details/presentation/components/SunnyBackground.kt
+++ app/src/main/java/com/an/diaryapp/feature_note_details/presentation/components/SunnyBackground.kt
@@ -1,17 +1,35 @@
 package com.an.diaryapp.feature_note_details.presentation.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.an.diaryapp.R
 
 @Composable
 fun SunnyBackground() {
@@ -21,12 +39,46 @@ fun SunnyBackground() {
     val screenHeight = configuration.screenHeightDp
     val screenWidth = configuration.screenWidthDp
 
+    val rotationDurationMillis: Int = 25000
+    val rotationDegrees: Float = 360f
+
+    val translationDurationMillis: Int = 2000
+    val translationDistance: Dp = 100.dp
+
+    var rotation by remember { mutableStateOf(0f) }
+
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val rotationState by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = rotationDegrees,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = rotationDurationMillis,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    val translationState by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = translationDistance.value,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = translationDurationMillis,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .drawBehind {
 
-               /* val path = Path().apply {
+                /* val path = Path().apply {
                     lineTo(screenWidth.toFloat(), 100F)
                 }*/
                 drawRect(
@@ -34,19 +86,27 @@ fun SunnyBackground() {
                 )
                 //drawPath(path = path, color = Color.White)
                 drawCircle(
-                    radius = 100f,
+                    radius = screenWidth.toFloat() * 3 / 2,
                     color = Color.Yellow,
                     center = Offset(
-                        x = screenWidth.toFloat()*2,
-                        y = screenWidth.toFloat()
+                        x = screenWidth.toFloat() * 3,
+                        y = 0f
                     )
                 )
 
 
-
-
             }
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_sunny),
+            contentDescription = null,
+            modifier = Modifier
+                .size((screenWidth / 5 * 2).dp)
+                .rotate(rotationState)
+                .align(Alignment.TopEnd)
+
+        )
 
     }
 }
